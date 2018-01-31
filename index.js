07a311ae1533b577047f452018b51a74a4c84ebf
Nick Leake <leakend@dukes.jmu.edu>
2018-01-30 20:43:33 -0500

    add game functionality for UI

--- index.js
+++ index.js
@@ -1,76 +1,119 @@
 const rockButton = document.getElementById("rock");
 const paperButton = document.getElementById("paper");
 const scissorsButton = document.getElementById("scissors");
-const score = document.getElementById("score");
+const scoreView = document.getElementById("score");
 const results = document.getElementById("results");
+const roundView = document.getElementById("round");
+const cpuChoice = document.getElementById("cpuChoice");
+const newGameButton = document.getElementById("newGame");
 let gameOver = false;
-let round = 0;
+let round = 1;
 let score = {
   player: 0,
   computer: 0
 };
 
-rockButton.addEventListener("click", playRound("rock"));
-paperButton.addEventListener("click", playRound("paper"));
-scissorsButton.addEventListener("click", playRound("scissors"));
+rockButton.addEventListener("click", throwRock);
+paperButton.addEventListener("click", throwPaper);
+scissorsButton.addEventListener("click", throwScissors);
+newGameButton.addEventListener("click", newGame)
 
 function newGame(){
   gameOver = false;
-  round = 0;
-  updateScore();
+  round = 1;
+  score.player = 0;
+  score.computer = 0;
+  updateView();
   clearResults();
+  rockButton.disabled = false;
+  paperButton.disabled = false;
+  scissorsButton.disabled = false;
 }
 
 function clearResults(){
   results.innerHTML = "";
+  cpuChoice.innerHTML = "";
 }
 
-function updateScore(){
+function updateView(){
   playerScore.innerHTML = score.player;
   cpuScore.innerHTML = score.computer;
+  roundView.innerHTML = round;
 }
 
 function computerPlay(){
   let choices = ["rock", "paper", "scissors"];
   return choices[Math.floor(Math.random() * 3)];
+  cpuChoice.innerHTML = `Computer throws ${computerSelection}`;
 }
 
-function playRound(playerSelection){
-  let winner = "";
+function throwScissors(){
+  clearResults();
   let computerSelection = computerPlay();
-  if(playerSelection === "rock"){
-    if(computerSelection === "scissors"){
-      winner = "player";
-    } else if (computerSelection === "rock") {
-      winner = "tie";
-    } else {
-      winner = "computer";
-    }
-  } else if(playerSelection === "paper"){
-    if(computerSelection === "rock"){
-      winner = "player"
-    } else {
-      winner = "computer"
-    }
-  } else if(playerSelection === "scissors"){
-    if(computerSelection === "paper"){
-      winner = "player"
-    } else {
-      winner = "computer"
-    }
+  if(computerSelection === "paper"){
+    playerWins()
+  } else if(computerSelection === "scissors"){
+    tie();
+  } else {
+    cpuWins();
   }
-  return winner;
-  updateScore();
 }
 
-function game(){
-  let numrounds = 5;
-  for (let index = 0; index < numrounds; index++) {
-    let winRound = playRound();
-    winRound === "player" ? score.player += 1 : score.computer += 1;
+function throwRock(){
+  clearResults();
+  let computerSelection = computerPlay();
+  if(computerSelection === "scissors"){
+    playerWins();
+  } else if (computerSelection === "rock") {
+    tie();
+  } else {
+    cpuWins();
   }
-  let winner = score.player > score.computer ? "player" : "computer";
-  return `${winner} wins!`;
+}
+
+function throwPaper(){
+  clearResults();
+  let computerSelection = computerPlay();
+  if(computerSelection === "rock"){
+    playerWins();
+  } else if (computerSelection === "paper"){
+    tie();
+  } else {
+    cpuWins();
+  }
+}
+
+function tie(){
+  round++;
+  results.innerHTML = "This round is a tie.";
+  updateView();
+  checkWinner();
+}
+
+function playerWins(){
+  round++;
+  score.player++;
+  results.innerHTML = "You win this round.";
+  updateView();
+  checkWinner();
+}
+
+function cpuWins(){
+  round++;
+  score.computer++;
+  results.innerHTML = "You lose this round.";
+  updateView();
+  checkWinner();
+}
+
+function checkWinner(){
+  if (round > 5){
+    let winner = score.player > score.computer ? "player" : "computer";
+    results.innerHTML = `${winner} wins the game! Click New Game to play again.`
+    rockButton.disabled = true;
+    paperButton.disabled = true;
+    scissorsButton.disabled = true;
+  }
 }
 
 
